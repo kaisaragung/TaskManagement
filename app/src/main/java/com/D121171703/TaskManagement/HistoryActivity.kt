c0bb909a8e9c900389a4a11495f96fe846ebdd10
kaisaragung <kayzaragung1307@gmail.com>
2022-12-14 20:28:43 +0800

    Mobile

---
+++ app/src/main/java/com/D121171703/TaskManagement/HistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.D121171703.TaskManagement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HistoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+    }
+}
